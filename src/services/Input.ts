3cf13cc6cbbe857d2506bad01567f90937cdff65
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2025-06-28 12:49:08 -0300

    chore: Initial files

---
+++ src/services/Input.ts
@@ -0,0 +1,7 @@
+export default function Input() {
+    const elements = {
+        day: document.querySelector('input#day') as HTMLInputElement,
+        month: document.querySelector('input#month') as HTMLInputElement,
+        year: document.querySelector('input#year') as HTMLInputElement
+    };
+}
